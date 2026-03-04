a303b712d1d955ccb97ea8d9603edaaba2daecea
Ambrose <ambymbayi45@gmail.com>
2026-03-05 00:40:11 +0300

    fixed updates

--- src/app/components/pages/settings/branding/branding.component.ts
+++ src/app/components/pages/settings/branding/branding.component.ts
@@ -25,9 +25,9 @@ interface ThemeConfig {
 
 @Component({
   selector: 'app-branding',
-  standalone: false,
   templateUrl: './branding.component.html',
-  styleUrls: ['./branding.component.scss']
+  styleUrls: ['./branding.component.scss'],
+  standalone: false,
 })
 export class BrandingComponent implements OnInit, OnDestroy {
   themeForm!: FormGroup;
