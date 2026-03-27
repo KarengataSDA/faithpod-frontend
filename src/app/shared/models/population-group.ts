c04ed66322074ed959b11ed3cf3a4dbaf36020c5
Ambrose <ambymbayi45@gmail.com>
2026-03-27 15:30:01 +0300

    fixed minor updates

--- src/app/shared/models/population-group.ts
+++ src/app/shared/models/population-group.ts
@@ -3,5 +3,6 @@ import { Member } from "./member"
 export interface PopulationGroup {
     id: number
     name: string
+    members_count?: number
     members: Member[]
 }
--- src/app/shared/models/prayercell.ts
+++ src/app/shared/models/prayercell.ts
@@ -3,5 +3,6 @@ import { Member } from "./member"
 export interface Prayercell {
     id: number
     name: string
+    members_count?: number
     members: Member[]
 }
